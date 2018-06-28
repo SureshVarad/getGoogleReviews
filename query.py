131ce369311ef24760d05bd06a618e3429a00436
Suresh Varad <sureshkumarv@live.com>
2018-06-28 22:22:30 +0000

    Reformated code and adding logging.

--- query.py
+++ query.py
@@ -3,42 +3,57 @@ Program to invoke Google Places API and return results
 
 @author: Suresh
 """
+# Imports
 from __future__ import absolute_import
+import os
+import sys
+import warnings
+import logging
+#import pdb
 from decimal import Decimal
 from collections import defaultdict
 from google.cloud import language
 from google.cloud.language import enums
 from google.cloud.language import types as google_types
-import warnings
-import os
-
 from googleplaces import GooglePlaces, types, lang
-#from . import googleplaces, types, lang
 
 __all__ = ['GooglePlaces', 'GooglePlacesError', 'GooglePlacesAttributeError', 'geocode_location']
 
-if (os.name == "posix"):
-    os.system("clear")
-else:
-    os.system("cls")
+#Set the Logs for debugging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+# create a file handler
+fileHandler = logging.FileHandler('googleReviews.log')
+fileHandler.setLevel(logging.INFO)
+# create a logging format
+formatter = logging.Formatter('%(asctime)s %(filename)s:%(lineno)d [%(levelname)s] -> %(message)s')
+fileHandler.setFormatter(formatter)
+# add the handlers to the logger
+logger.addHandler(fileHandler)
 
-geoLocation = ""
+geoLocation = "" 
 
 # [START def_getInput]
 def getInput():
-	""" Gets City/State or Zip option from user"""
+	'''Gets City/State or Zip option from user'''
+
+	#clear the screen
+	if (os.name == "posix"): #Mac/Linux
+	    os.system("clear")
+	else:
+	    os.system("cls")
 
 	print "\n\n\n\t\tWelcome to 7-Eleven\n\n\n"
 	print "\n\tTo view the nearest stores, please provide one of following"
 	print "\n1. City, State"
 	print "\n2. Zip"
-	inputOption = raw_input('\nYour Choice (1/2): ')
+	inputOption = raw_input('\nYour Choice (1 or 2): ')
 	validateInput(inputOption)
 # [END def_getInput]
 
 # [START def_validateInput]
 def validateInput(inputOption):
-	""" Fetches & Validates the passed input and gets City/Zip details"""
+	'''Fetches & Validates the passed input and gets City/Zip details'''
 	global geoLocation
   	cityState = ""
 	zipCode = ""
@@ -66,7 +81,8 @@ def print_results(place, detail_res, id):
     #print "Phone # : %s " % str(place.local_phone_number)
     #print "Website : %s " % str(place.website)
     #print "MapsURL : %s \n\n" % str(place.url)
-    print('Detailed Results {}'.format(detail_res[id]))
+    #print('Detailed Results {}'.format(detail_res[id]))
+    logger.info('Detailed Results {}'.format(detail_res[id]))
 # [END of def_print_results]
 
 # [START MAIN BLOCK]
@@ -75,10 +91,9 @@ if __name__ == '__main__':
 	print "\n\nGeo Location: " + geoLocation
 
 
-	YOUR_API_KEY = 'AIzaSyAG2gC75WBWAtPsJ9dFtzum9yts0xlZ3Qo'
+	YOUR_API_KEY = ""
 
 	google_places = GooglePlaces(YOUR_API_KEY)
-	#google_places = googleplaces.GooglePlaces(YOUR_API_KEY)
 
 	#Get All 7 Eleven Stores of type "Gas Station" in the radius of 25miles. Google Returns only 20 top results in one page.
 	query_result = google_places.nearby_search( location=geoLocation, keyword='7 Eleven', radius=25000, types=[types.TYPE_GAS_STATION])
@@ -91,47 +106,60 @@ if __name__ == '__main__':
 	#Initialize Google Cloud Sentiment Analysis
 	client = language.LanguageServiceClient()
 
-	for place in query_result.places:
-	    # The following method has to make a further API call, before getting reviews.
-	    place.get_details()
-	    id = place.place_id
-	    address = place.details.get('formatted_address')
-	    ratings = place.rating
-	    reviews = place.details.get('reviews')
-	    #reviews = place.details.get('reviews').get('text')
-	    reviewText = []  
-	    reviewRatings = []
-	    positiveReviews = []
-	    negativeReviews = []
-	    #'''
-	    for i, _ in enumerate(reviews):
-		reviewTextBuffer = reviews[i]['text']
-		rate = reviews[i]['rating']
-		reviewTextBuffer = reviewTextBuffer.encode('utf-8')
-		reviewText.append(reviewTextBuffer)
-		reviewRatings.append(rate)
-		
-		#Get Google Sentiment Analysis for each review
-		document = google_types.Document(content=reviewTextBuffer, type=enums.Document.Type.PLAIN_TEXT)
-		annotations = client.analyze_sentiment(document=document)
-		score = annotations.document_sentiment.score
-		magnitude = annotations.document_sentiment.magnitude
-		# If score is above 0.3 it's a positive review, we can display this to customers
-		# We are going to skip over the mixed reviews for now.
-		# but can easily add an else condition, to capture them if required.
-		if score > 0.3:
-			positiveReviews.append(reviewTextBuffer)	
-		elif score < -0.3:
-			negativeReviews.append(reviewTextBuffer)	
-	    #'''
-	    detail_res[id]['overallRatings'] = ratings
-	    detail_res[id]['reviewRatings'] = reviewRatings
-	    detail_res[id]['AllReviewText'] = reviewText
-	    detail_res[id]['positiveReviews'] = positiveReviews
-	    detail_res[id]['negativeReviews'] = negativeReviews
-	    print_results(place, detail_res, id)
-	    count =  count + 1
-	    pass
+	#pdb.set_trace()
+	try:
+		for place in query_result.places:
+		    # The following method has to make a further API call, before getting reviews.
+		    place.get_details()
+		    id = place.place_id
+		    address = place.details.get('formatted_address')
+		    ratings = place.rating
+		    reviews = place.details.get('reviews')
+		    #reviews = place.details.get('reviews').get('text')
+		    reviewText = []  
+		    reviewRatings = []
+		    positiveReviews = []
+		    negativeReviews = []
+		    try:
+		    	for i, _ in enumerate(reviews):
+			   reviewTextBuffer = reviews[i]['text']
+			   rate = reviews[i]['rating']
+			   reviewTextBuffer = reviewTextBuffer.encode('utf-8')
+			   reviewText.append(reviewTextBuffer)
+			   reviewRatings.append(rate)
+			
+			   #Get Google Sentiment Analysis for each review
+			   document = google_types.Document(content=reviewTextBuffer, type=enums.Document.Type.PLAIN_TEXT)
+			   annotations = client.analyze_sentiment(document=document)
+			   score = annotations.document_sentiment.score
+			   #magnitude = annotations.document_sentiment.magnitude
+			   logger.info("Google Sentiment Score = {}".format(score))
+
+			   # If score is above 0.3 it's a positive review, we can display this to customers
+			   # We are going to skip over the mixed reviews for now.
+			   # but can easily add an else condition, to capture them if required.
+			   if score > 0.3:
+				positiveReviews.append(reviewTextBuffer)	
+			   elif score < -0.3:
+				negativeReviews.append(reviewTextBuffer)	
+			#End of For loop, for fetching reviews
+		    except:
+			#eprint("Exception inside fetching reviews & sentiment, ", id, place.url, score, sep="---") 
+			print("Exception inside fetching reviews & sentiment ID:{}, Maps URL: {}, Score {}".format(id, place.url, score) )
+			pass
+		    detail_res[id]['overallRatings'] = ratings
+		    detail_res[id]['reviewRatings'] = reviewRatings
+		    detail_res[id]['AllReviewText'] = reviewText
+		    detail_res[id]['positiveReviews'] = positiveReviews
+		    detail_res[id]['negativeReviews'] = negativeReviews
+		    print_results(place, detail_res, id)
+		    count =  count + 1
+		    pass
+		#End of For loop for all query results on first page
+	except:
+		#eprint("Exception inside main for loop to fetch results from query", sep="---") 
+		print("Exception inside main for loop to fetch results from query")
+		pass
 	print "Total Count : %s " % count
 
 #[ END OF MAIN BLOCK AND PROGRRAM]
