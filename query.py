3a2b5529d8c789cb127e6d22b1db110116cafe88
Suresh Varad <sureshkumarv@live.com>
2018-06-29 13:17:50 +0000

    Updated sorting logic and added error checks

--- query.py
+++ query.py
@@ -9,15 +9,17 @@ import os
 import sys
 import warnings
 import logging
+import operator
 #import pdb
 from decimal import Decimal
 from collections import defaultdict
 from google.cloud import language
 from google.cloud.language import enums
 from google.cloud.language import types as google_types
-from googleplaces import GooglePlaces, types, lang
+from googleplaces import GooglePlaces, GooglePlacesError, GooglePlacesAttributeError, types, lang
+from operator import itemgetter
 
-__all__ = ['GooglePlaces', 'GooglePlacesError', 'GooglePlacesAttributeError', 'geocode_location']
+#__all__ = ['GooglePlaces', 'GooglePlacesError', 'GooglePlacesAttributeError', 'geocode_location']
 
 geoLocation = "" 
 log_dir = "./"
@@ -62,6 +64,11 @@ def getInput():
 	validateInput(inputOption)
 # [END def_getInput]
 
+# [START def_validateZipCode]
+def validateZipCode(zipCode):
+    return(len(zipCode) == 5 and zipCode.isdigit()) 
+# [END def_validateZipCode]
+
 # [START def_validateInput]
 def validateInput(inputOption):
 	'''Fetches & Validates the passed input and gets City/Zip details'''
@@ -78,44 +85,81 @@ def validateInput(inputOption):
 	         geoLocation = cityState
 	    else:
 		 zipCode = raw_input("Please enter Zip Code (5-Digits): ")
-		 #TODO: Validate Zip code format
-	         geoLocation = zipCode
-	print "\n\nGeoLocation from User Input : " + geoLocation
+		 #Validate Zip code format
+    		 zipCode = zipCode.replace(" ", "")
+		 if validateZipCode(zipCode) is True:
+	            geoLocation = zipCode
+		 else:
+		    print "\n\nInvalid ZipCode Format, should be only 5 digits, retry"
+		    validateInput(inputOption)
+	logging.debug("\n\nGeoLocation from User Input : {} ".format(geoLocation))
 # [END def_validateInput]
 
+
+# [START Of def_sortAndPrint]]
+def sortAndPrint(queryResults):
+   logging.debug("UnSorted Results : {}".format(queryResults))
+   queryResults.sort(key=itemgetter('score','overAllRatings'), reverse=True)
+   logging.debug("Sorted Results : {}".format(queryResults))
+   print_results(queryResults)
+# [END def_sortAndPrint]]
+
+
 # [START Of def_print_results]
-def print_results(place, detail_res, id):
-    print "%s @ %s, Google Place ID: %s" % (place.name, address, id)
-    print "Ratings : %s <--> %s \n" % (str(ratings), str(reviewRatings))
-    print "Positive Reviews : %s \n" % str(positiveReviews)
-    print "Negative Reviews : %s \n" % str(negativeReviews)
-    #print "Phone # : %s " % str(place.local_phone_number)
-    #print "Website : %s " % str(place.website)
-    #print "MapsURL : %s \n\n" % str(place.url)
-    #print('Detailed Results {}'.format(detail_res[id]))
-    logging.debug('Detailed Results {}'.format(detail_res[id]))
+def print_results(results):
+    printCounter = 1
+    for item in results:
+	print "\n\n\nSorted :%d/%d, %s @ %s, Google Place ID: %s" % (printCounter, len(results), item['placeName'], item['address'], item['placeId'])
+	print "Phone # : %s " % str(item['phoneNumber'])
+	print "MapsURL : %s " % (item['mapsUrl'])
+	print "Combined Sentiment Score : {0:.9f}".format((round(item['score'],9)))
+	print "Ratings : %s " % (str(item['overAllRatings']))
+	print "Positive Reviews -----> %s " % item['positiveReviews']
+	#print "Negative Reviews : %s " % item['negativeReviews']
+	#print('Detailed Results {}'.format(details))
+	logging.debug('Detailed Printed Results {},{},{},{},{}'.format(item['placeId'], 
+			item['address'],item['phoneNumber'],round(item['score'],9),item['overAllRatings']))
+	printCounter += 1
+	#After 4 results Take a break and let user review them before continuing to next set of results.
+	if printCounter % 4 is 0:
+	   try:
+		input("\n\nPress Enter to continue...")
+	   except SyntaxError:
+    		pass
+    #End of For Loop
+    print "\n\n"
 # [END of def_print_results]
 
 # [START MAIN BLOCK]
 if __name__ == '__main__':
 	initialize_logger(log_dir, file_name)
 	getInput()
-	print "\n\nGeo Location: " + geoLocation
+	print("\n\nGeoLocation: {}".format(geoLocation))
 
-	YOUR_API_KEY = "AIzaSyAG2gC75WBWAtPsJ9dFtzum9yts0xlZ3Qo"
+	YOUR_API_KEY = ""
 
 	google_places = GooglePlaces(YOUR_API_KEY)
 
-	#Get All 7 Eleven Stores of type "Gas Station" in the radius of 25miles. Google Returns only 20 top results in one page.
-	query_result = google_places.nearby_search( location=geoLocation, keyword='7 Eleven', radius=25000, types=[types.TYPE_GAS_STATION])
+	try:
+	    #
+	    #Get All 7 Eleven Stores of type "Gas Station" in the radius of 25miles. Google Returns only 20 top results in one page.
+	    query_result = google_places.nearby_search( location=geoLocation, keyword='7 Eleven', 
+							radius=25000, types=[types.TYPE_GAS_STATION])
+	except Exception as err:
+		logging.fatal(err, exc_info=False)
+		logging.debug("Exception occured when fetching results from Google")
+		print("Exception occured when fetching results based on input, Exiting..!!")
+		sys.exit(1)
+	#End of try & Except
 
 	if query_result.has_attributions:
 	    print query_result.html_attributions
 
-	detail_res = defaultdict(dict)
 	count = 0
 	#Initialize Google Cloud Sentiment Analysis
 	client = language.LanguageServiceClient()
+	#Create a List to store query details below
+	queryResults = []
 
 	#pdb.set_trace()
 	try:
@@ -124,64 +168,82 @@ if __name__ == '__main__':
 		    place.get_details()
 		    id = place.place_id
 		    address = place.details.get('formatted_address')
-		    ratings = place.rating
+		    overAllRatings = place.rating
 		    reviews = place.details.get('reviews')
 		    #reviews = place.details.get('reviews').get('text')
 		    reviewText = []  
-		    reviewRatings = []
+		    #reviewRatings = []
 		    positiveReviews = []
 		    negativeReviews = []
 		    combinedSentimentScore = 0.0
     		    logging.debug("%s @ %s, Google Place ID: %s" % (place.name, address, id))
-		    try:
-		    	for i, _ in enumerate(reviews):
-			   #Reset score, just to make sure it's not carrying forward old data.
-			   score=0.0
-			   reviewTextBuffer = reviews[i]['text']
-			   rate = reviews[i]['rating']
-			   reviewTextBuffer = reviewTextBuffer.encode('utf-8')
-			   reviewText.append(reviewTextBuffer)
-			   reviewRatings.append(rate)
-			
-			   #Get Google Sentiment Analysis for each review
-			   document = google_types.Document(content=reviewTextBuffer, type=enums.Document.Type.PLAIN_TEXT)
-			   annotations = client.analyze_sentiment(document=document)
-		      	   #Get the overall score for the review submitted.
-			   score = annotations.document_sentiment.score 
-			   #Get the magnitude for the review submitted.
-			   #magnitude = annotations.document_sentiment.magnitude 
-			   logging.debug("Google Place Review = {}".format(reviewTextBuffer))
-			   logging.debug("Google Sentiment Score = {}".format(score))
+		    #If there are no reviews, skip drilling down into details 
+		    logging.debug("Id: {}, Address {}, URL {}".format(id,address,place.url))
+		    if reviews is not None:
+			try:
+			   for i, _ in enumerate(reviews):
+				#Reset score, just to make sure it's not carrying forward old data.
+				score=0.0
+				reviewTextBuffer = reviews[i]['text']
+				#rate = reviews[i]['rating']
+				reviewTextBuffer = reviewTextBuffer.encode('utf-8')
+				reviewText.append(reviewTextBuffer)
+				#reviewRatings.append(rate)
+				
+				#Get Google Sentiment Analysis for each review
+				document = google_types.Document(content=reviewTextBuffer, type=enums.Document.Type.PLAIN_TEXT)
+				annotations = client.analyze_sentiment(document=document)
+				#Get the overall score for the review submitted.
+				score = annotations.document_sentiment.score 
+				#Get the magnitude for the review submitted.
+				#magnitude = annotations.document_sentiment.magnitude 
+				logging.debug("Google Place Review = {}".format(reviewTextBuffer))
+				logging.debug("Google Sentiment Score = {}".format(score))
 
-			   # Google's sentiment score ranges between -1.0 to 1.0
-			   # If score is above 0.3 it's a positive review, we can display this to customers
-			   # We are going to skip over the mixed reviews for now.
-			   # but can easily add an else condition, to capture them if required.
-			   combinedSentimentScore += score
-			   if score > 0.3:
-				positiveReviews.append(reviewTextBuffer)
-			   elif score < -0.3:
-				negativeReviews.append(reviewTextBuffer)
-			#End of For loop, for fetching reviews
-		    except Exception, err:
-			print Exception, err
-			print("Exception inside fetching reviews & sentiment ID:{}, Maps URL: {}, Score {}".format(id, place.url, score) )
-			pass
-		    detail_res[id]['overallRatings'] = ratings
-		    detail_res[id]['reviewRatings'] = reviewRatings
-		    detail_res[id]['score'] = combinedSentimentScore 
-		    #detail_res[id]['AllReviewText'] = reviewText
-		    detail_res[id]['positiveReviews'] = positiveReviews
-		    detail_res[id]['negativeReviews'] = negativeReviews
-		    print_results(place, detail_res, id)
-    		    logging.debug("\n\nAll Details fetched so far ===> %s\n\n\n" % (detail_res))
+				# Google's sentiment score ranges between -1.0 to 1.0
+				# If score is above 0.3 it's a positive review, we can display this to customers
+				# We are going to skip over the mixed reviews for now.
+				# but can easily add an else condition, to capture them if required.
+				combinedSentimentScore += score
+				if score > 0.3:
+					positiveReviews.append(reviewTextBuffer)
+				elif score < -0.3:
+					negativeReviews.append(reviewTextBuffer)
+				#End of For loop, for fetching reviews
+			except Exception as err:
+				# If it failed, Most cases its just mean NO Reviews are present for this store. Log it and continue
+				# If Google Sentiments call failed, then it would assign default sentiment score of 0.0 for all stores, 
+				# which would cause algorithm to use google ratings for sorting
+				logging.fatal(err, exc_info=True)
+				logging.debug("Exception inside fetching reviews & sentiment ID:{}, Maps URL: {}, Score {}".format(id, place.url, score) )
+				pass
+		    #End of -- If Loop for len(reviews) > 0 
+		    details = {
+			'placeId': id,
+			'placeName': place.name,
+			'overAllRatings': overAllRatings,
+			'score': combinedSentimentScore,
+			'address': address,
+			'phoneNumber': place.local_phone_number,
+			'mapsUrl': place.url,
+			'positiveReviews': positiveReviews,
+			'negativeReviews': negativeReviews
+		    }
+		    queryResults.append(details)
+		    #print_results(place, details, id)
+    		    logging.debug("\n\nAll Details fetched so far ===> %s\n\n\n" % (queryResults))
 		    count =  count + 1
 		    pass
 		#End of For loop for all query results on first page
-	except Exception, err:
-		print Exception, err
-		print("Exception inside main for loop to fetch results from query")
+	except Exception as err:
+		#print Exception, err
+		logging.fatal(err, exc_info=True)
+		logging.debug("Exception occured when fetching results from query")
+		print("Exception occured when fetching results from query")
 		pass
-	print "Total Count : %s " % count
+	print "Total Fetched Stores for this GeoLocation : %s " % count
+	
+	#Let's Sort based on combinedSentimentScore & overAllRatings
+	sortAndPrint(queryResults)
 
 #[ END OF MAIN BLOCK AND PROGRRAM]
